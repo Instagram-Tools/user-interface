9a6de751af4f20747c82282786ecf5543eab8cc1
Gott50 <xxGott50xx@gmail.com>
2018-06-08 18:07:48 +0200

    use NGINX Port 50 to interact with the API

--- src/API_Gateway.js
+++ src/API_Gateway.js
@@ -1,6 +1,6 @@
 export default class API_Gateway {
   static async get(userName) {
-    let url = 'localhost/api/?user=' + userName;
+    let url = 'http://localhost:50/?user=' + userName;
     console.log(url);
     try {
       let response = await fetch(url);

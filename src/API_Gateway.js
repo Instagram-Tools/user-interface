3942896d56d4402b8be9f7d560943860a28fa85d
Gott50 <xxGott50xx@gmail.com>
2018-06-08 18:22:00 +0200

    extract const API_URL

--- src/API_Gateway.js
+++ src/API_Gateway.js
@@ -1,9 +1,19 @@
+const API_URL = 'http://localhost:50/';
 export default class API_Gateway {
   static async get(userName) {
-    let url = 'http://localhost:50/?user=' + userName;
+    let url = API_URL + '?user=' + userName;
     console.log(url);
     try {
       let response = await fetch(url);
+      return await response.text();
+    } catch (e) {
+      return e.message;
+    }
+  }
+
+  static async put(user, timetable) {
+    try {
+      let response = await fetch(API_URL);
       return await response.text();
     } catch (e) {
       return e.message;

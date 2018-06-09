1585da6e58568d9963c7f24033594cb495744f4d
Gott50 <xxGott50xx@gmail.com>
2018-06-09 14:34:16 +0200

    reject on Error in API_Gateway at get(), put()

--- src/API_Gateway.js
+++ src/API_Gateway.js
@@ -3,26 +3,22 @@ export default class API_Gateway {
   static async get(userName) {
     let url = API_URL + '?user=' + userName;
     console.log(url);
-    try {
-      let response = await fetch(url);
-      return await response.text();
-    } catch (e) {
-      return e.message;
-    }
+    let response = await fetch(url);
+    let text = await response.text();
+    if (response.status === 200) return text;
+    else return Promise.reject(new Error(text));
   }
 
   static async put(data) {
-    try {
-      let response = await fetch(API_URL, {
-        method: 'PUT',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      return await response.text();
-    } catch (e) {
-      return e.message;
-    }
+    let response = await fetch(API_URL, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    let text = await response.text();
+    if (response.status === 200) return text;
+    else return new Error(text);
   }
 }

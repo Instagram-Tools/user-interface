9296466e29b300082bfc8e1d3fcaeea6fa0c1904
Gott50 <xxGott50xx@gmail.com>
2018-06-11 12:29:22 +0200

    add GeneralSettings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
+import GeneralSettings from './GeneralSettings';
 import './webflow.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div>
         <NavBar />
+        <GeneralSettings />
       </div>
     );
   }

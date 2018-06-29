814dc06c3f320387728ef9a50987e277912403df
Gott50 <xxGott50xx@gmail.com>
2018-06-29 12:47:17 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import NavBar from './NavBar';
 import './css/webflow.css';
 import './css/normalize.css';
 import './css/code-connect.webflow.css';
+import NavBar from './NavBar';
 import GeneralSettings from './GeneralSettings';
 
 class App extends Component {

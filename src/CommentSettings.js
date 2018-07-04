b9bdac13d6ba5fa4867048108b733a3f2edbcf87
Gott50 <xxGott50xx@gmail.com>
2018-07-05 00:34:54 +0200

    add comments

--- src/CommentSettings.js
+++ src/CommentSettings.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import TextFieldNumber from './TextFieldNumber';
+import ContextList from './ContextList';
+import TextFieldBroad from './TextFieldBroad';
 
 export default class CommentSettings extends Component {
   render() {
@@ -54,6 +56,55 @@ export default class CommentSettings extends Component {
                 max={100}
                 value="do_like_percentage"
               />
+
+              <div
+                data-w-id="1846180f-b0c1-4f62-a33b-df4641310d47"
+                className="settingsgroup"
+              >
+                <ContextList value="comments" iconClass="commenticon" />
+                <div className="columnholder hashtag_settings_holder">
+                  <TextFieldBroad
+                    value="comments"
+                    placeholder="Enter hashtags this way: fitness"
+                  />
+                  <div
+                    data-w-id="034888e9-27da-288f-a156-1f16d12dae01"
+                    className="suggestions"
+                  >
+                    <div className="suggestionscolumn">
+                      <div className="suggestion_text_hashtags">#abs</div>
+                      <div className="suggestion_text_hashtags">#gymrat</div>
+                      <div className="suggestion_text_hashtags">
+                        #fitnessworkout
+                      </div>
+                      <div className="suggestion_text_hashtags">#hashtag</div>
+                      <div className="suggestion_text_hashtags">
+                        #deinemudda
+                      </div>
+                    </div>
+                    <div className="suggestionscolumn">
+                      <div
+                        data-w-id="27ba4687-69d1-9219-0c36-8ae82b546770"
+                        className="amount_text_hashtags"
+                      >
+                        268.847.786 posts
+                      </div>
+                      <div className="amount_text_hashtags">
+                        123.456.789 posts
+                      </div>
+                      <div className="amount_text_hashtags">
+                        987.654.321 posts
+                      </div>
+                      <div className="amount_text_hashtags">
+                        50.123.456 posts
+                      </div>
+                      <div className="amount_text_hashtags">
+                        5.345.678 posts
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div className="titleholder">
                 <label htmlFor="email" className="settingtitle">
                   <strong>Filters:</strong> Comment pictures with...

c7cf96ad6c7fb908e45d07dff39fcd5d988ae9d6
Gott50 <xxGott50xx@gmail.com>
2018-07-07 12:03:07 +0200

    use Context instead of State

--- src/TimeTable.js
+++ src/TimeTable.js
@@ -2,58 +2,50 @@ import React from 'react';
 import { Context } from './Context';
 
 class TimetableWrapper extends React.Component {
-  state = {
-    clicked: []
-  };
-
   render() {
     return (
-      <Context.Consumer>
-        {context => (
-          <div>
-            <div className="legendholder horizontal">
-              <div className="legendtext horizontaltext">Mon</div>
-              <div className="legendtext horizontaltext">Tue</div>
-              <div className="legendtext horizontaltext">Wed</div>
-              <div className="legendtext horizontaltext">Thu</div>
-              <div className="legendtext horizontaltext">Fri</div>
-              <div className="legendtext horizontaltext">Sat</div>
-              <div className="legendtext horizontaltext">Sun</div>
-            </div>
-            <div className="actvityholder" style={{ alignItems: 'start' }}>
-              <div className="legendholder">
-                <div className="legendtext">1am</div>
-                <div className="legendtext">2</div>
-                <div className="legendtext">3</div>
-                <div className="legendtext">4</div>
-                <div className="legendtext">5</div>
-                <div className="legendtext">6</div>
-                <div className="legendtext">7</div>
-                <div className="legendtext">8</div>
-                <div className="legendtext">9</div>
-                <div className="legendtext">10</div>
-                <div className="legendtext">11</div>
-                <div className="legendtext">12</div>
-                <div className="legendtext">1pm</div>
-                <div className="legendtext">2</div>
-                <div className="legendtext">3</div>
-                <div className="legendtext">4</div>
-                <div className="legendtext">5</div>
-                <div className="legendtext">6</div>
-                <div className="legendtext">7</div>
-                <div className="legendtext">8</div>
-                <div className="legendtext">9</div>
-                <div className="legendtext">10</div>
-                <div className="legendtext">11‍</div>
-                <div className="legendtext">12‍</div>
-              </div>
-              <div className="tableholder" style={{ display: 'flex' }}>
-                {this.range(1, 7).map(this.mapDays.bind(this))}
-              </div>
-            </div>
+      <div>
+        <div className="legendholder horizontal">
+          <div className="legendtext horizontaltext">Mon</div>
+          <div className="legendtext horizontaltext">Tue</div>
+          <div className="legendtext horizontaltext">Wed</div>
+          <div className="legendtext horizontaltext">Thu</div>
+          <div className="legendtext horizontaltext">Fri</div>
+          <div className="legendtext horizontaltext">Sat</div>
+          <div className="legendtext horizontaltext">Sun</div>
+        </div>
+        <div className="actvityholder" style={{ alignItems: 'start' }}>
+          <div className="legendholder">
+            <div className="legendtext">1am</div>
+            <div className="legendtext">2</div>
+            <div className="legendtext">3</div>
+            <div className="legendtext">4</div>
+            <div className="legendtext">5</div>
+            <div className="legendtext">6</div>
+            <div className="legendtext">7</div>
+            <div className="legendtext">8</div>
+            <div className="legendtext">9</div>
+            <div className="legendtext">10</div>
+            <div className="legendtext">11</div>
+            <div className="legendtext">12</div>
+            <div className="legendtext">1pm</div>
+            <div className="legendtext">2</div>
+            <div className="legendtext">3</div>
+            <div className="legendtext">4</div>
+            <div className="legendtext">5</div>
+            <div className="legendtext">6</div>
+            <div className="legendtext">7</div>
+            <div className="legendtext">8</div>
+            <div className="legendtext">9</div>
+            <div className="legendtext">10</div>
+            <div className="legendtext">11‍</div>
+            <div className="legendtext">12‍</div>
           </div>
-        )}
-      </Context.Consumer>
+          <div className="tableholder" style={{ display: 'flex' }}>
+            {this.range(1, 7).map(this.mapDays.bind(this))}
+          </div>
+        </div>
+      </div>
     );
   }
 
@@ -75,36 +67,43 @@ class TimetableWrapper extends React.Component {
 
   mapHours(day, hour) {
     return (
-      <div
-        onClick={(() => this.mark(day, hour)).bind(this)}
-        onDragEnter={(() => this.mark(day, hour)).bind(this)}
-        draggable="true"
-        className={
-          'week_schedule_column ' +
-          (day % 2 ? '' : '_2nd') +
-          (this.contains(
-            this.state.clicked,
-            this.format(new Date(4, 1, day, hour))
-          )
-            ? ' ifclicked'
-            : '')
-        }
-      />
+      <Context.Consumer>
+        {context => (
+          <div
+            onClick={(() => this.mark(context, day, hour)).bind(this)}
+            onDragEnter={(() => this.mark(context, day, hour)).bind(this)}
+            draggable="true"
+            className={
+              'week_schedule_column ' +
+              (day % 2 ? '' : '_2nd') +
+              (this.contains(
+                context.state.clicked,
+                this.format(new Date(4, 1, day, hour))
+              )
+                ? ' ifclicked'
+                : '')
+            }
+          />
+        )}
+      </Context.Consumer>
     );
   }
 
-  mark(day, hour) {
-    this.setState(p => ({
+  mark(context, day, hour) {
+    context.setState(p => ({
       clicked: this.contains(
-        this.state.clicked,
+        context.state.clicked,
         this.format(new Date(4, 1, day, hour))
       )
         ? p.clicked
-        : [...p.clicked, this.format(new Date(4, 1, day, hour))]
+        : [
+            ...(p.clicked ? p.clicked : []),
+            this.format(new Date(4, 1, day, hour))
+          ]
     }));
   }
 
-  contains(list, o) {
+  contains(list = [], o) {
     return list.map(e => JSON.stringify(e)).includes(JSON.stringify(o));
   }
 

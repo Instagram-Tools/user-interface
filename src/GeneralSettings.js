5ea1dab0ddf1588b3db4c5fa7514f93b1449adac
Gott50 <xxGott50xx@gmail.com>
2018-07-17 08:35:03 +0200

    display Suggestions on received Suggestions

--- src/GeneralSettings.js
+++ src/GeneralSettings.js
@@ -45,42 +45,6 @@ export default class GeneralSettings extends Component {
                     query="hashtags"
                     placeholder="Enter hashtags this way: fitness"
                   />
-                  <div
-                    data-w-id="034888e9-27da-288f-a156-1f16d12dae01"
-                    className="suggestions"
-                  >
-                    <div className="suggestionscolumn">
-                      <div className="suggestion_text_hashtags">#abs</div>
-                      <div className="suggestion_text_hashtags">#gymrat</div>
-                      <div className="suggestion_text_hashtags">
-                        #fitnessworkout
-                      </div>
-                      <div className="suggestion_text_hashtags">#hashtag</div>
-                      <div className="suggestion_text_hashtags">
-                        #deinemudda
-                      </div>
-                    </div>
-                    <div className="suggestionscolumn">
-                      <div
-                        data-w-id="27ba4687-69d1-9219-0c36-8ae82b546770"
-                        className="amount_text_hashtags"
-                      >
-                        268.847.786 posts
-                      </div>
-                      <div className="amount_text_hashtags">
-                        123.456.789 posts
-                      </div>
-                      <div className="amount_text_hashtags">
-                        987.654.321 posts
-                      </div>
-                      <div className="amount_text_hashtags">
-                        50.123.456 posts
-                      </div>
-                      <div className="amount_text_hashtags">
-                        5.345.678 posts
-                      </div>
-                    </div>
-                  </div>
                 </div>
               </div>
               <div className="titleholder">
--- src/TextFieldBroad.js
+++ src/TextFieldBroad.js
@@ -3,20 +3,53 @@ import { Context } from './Context';
 import API_Gageway from './API_Gateway';
 
 export default class TextFieldBroad extends Component {
+  state = {
+    suggestions: []
+  };
+
   render() {
     return (
       <Context.Consumer>
         {context => (
-          <input
-            type="text"
-            className="text-field broadfield w-input"
-            maxLength="256"
-            placeholder={this.props.placeholder}
-            onKeyPress={(e =>
-              this.pressEnter(e)
-                ? this.submitText(e, context)
-                : this.suggest(e.target.value)).bind(this)}
-          />
+          <div>
+            <input
+              type="text"
+              className="text-field broadfield w-input"
+              maxLength="256"
+              placeholder={this.props.placeholder}
+              onKeyPress={(e =>
+                this.pressEnter(e)
+                  ? this.submitText(e, context)
+                  : this.suggest(e.target.value)).bind(this)}
+            />
+            <div
+              style={{
+                display: this.state.suggestions.length > 0 ? 'flex' : 'none'
+              }}
+              data-w-id="034888e9-27da-288f-a156-1f16d12dae01"
+              className="suggestions"
+            >
+              <div className="suggestionscolumn">
+                <div className="suggestion_text_hashtags">#abs</div>
+                <div className="suggestion_text_hashtags">#gymrat</div>
+                <div className="suggestion_text_hashtags">#fitnessworkout</div>
+                <div className="suggestion_text_hashtags">#hashtag</div>
+                <div className="suggestion_text_hashtags">#deinemudda</div>
+              </div>
+              <div className="suggestionscolumn">
+                <div
+                  data-w-id="27ba4687-69d1-9219-0c36-8ae82b546770"
+                  className="amount_text_hashtags"
+                >
+                  268.847.786 posts
+                </div>
+                <div className="amount_text_hashtags">123.456.789 posts</div>
+                <div className="amount_text_hashtags">987.654.321 posts</div>
+                <div className="amount_text_hashtags">50.123.456 posts</div>
+                <div className="amount_text_hashtags">5.345.678 posts</div>
+              </div>
+            </div>
+          </div>
         )}
       </Context.Consumer>
     );

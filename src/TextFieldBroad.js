fb73530483ef40b0678d245ab2f7867c953a3a3b
Gott50 <xxGott50xx@gmail.com>
2018-07-18 13:57:13 +0200

    display ProfilePicture in User Suggestion

--- src/TextFieldBroad.js
+++ src/TextFieldBroad.js
@@ -81,12 +81,25 @@ export default class TextFieldBroad extends Component {
           onClick={(() => this.clickSuggestion(suggestion.user.username)).bind(
             this
           )}
-          style={{ display: 'inline-flex', justifyContent: 'space-between' }}
+          style={{
+            display: 'inline-grid',
+            gridTemplateColumns: '40px auto auto'
+          }}
         >
+          <img
+            src={suggestion.user.profile_pic_url}
+            style={{
+              border: 'solid 1px #efefef',
+              borderRadius: '30px',
+              height: '30px',
+              margin: '0 10px 0 0',
+              width: '30px'
+            }}
+          />
           <div className="suggestion_text_hashtags">
             {suggestion.user.username}
           </div>
-          <div className="amount_text_hashtags">
+          <div className="amount_text_hashtags" style={{ textAlign: 'right' }}>
             {suggestion.user.follower_count} followers
           </div>
         </div>

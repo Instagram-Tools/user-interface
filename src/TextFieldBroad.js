c6d5535e38d09ca5adad75369242c712ce41c6ce
Gott50 <xxGott50xx@gmail.com>
2018-09-19 11:18:01 +0200

    fix TextFieldBroad.js by removing Suggestions

--- src/TextFieldBroad.js
+++ src/TextFieldBroad.js
@@ -1,64 +1,10 @@
-import React, { Component } from 'react';
-import { Context } from './Context';
-import API_Gageway from './API_Gateway';
+import React from 'react';
+import TextFieldSuggestion from './TextFieldSuggestion';
 
-export default class TextFieldBroad extends Component {
-  state = {
-    suggestions: [],
-    value: ''
-  };
-  separator = ' ';
-
-  render() {
-    return (
-      <Context.Consumer>
-        {context => (
-          <div>
-            <input
-              type="text"
-              className="text-field broadfield w-input"
-              maxLength="256"
-              placeholder={this.props.placeholder}
-              value={this.state.value}
-              onChange={e => this.setState({ value: e.target.value })}
-              onKeyPress={e =>
-                this.pressEnter(e) ? this.submitText(context) : null
-              }
-            />
-            <div
-              style={{
-                display:
-                  this.state.suggestions.length > 0 ? 'inline-grid' : 'none',
-                position: 'relative',
-                top: '-60px'
-              }}
-              className="suggestions"
-            />
-          </div>
-        )}
-      </Context.Consumer>
-    );
+export default class TextFieldBroad extends TextFieldSuggestion {
+  onInput(e) {
+    this.setState({ value: e.target.value });
   }
 
-  submitText(context) {
-    const value = this.state.value;
-    context.setState(p => ({
-      [this.props.value]: this.addValue(p[this.props.value], value)
-    }));
-    this.setState({ value: '' });
-  }
-
-  addValue(old = [], value) {
-    let newVar = [...old];
-    value
-      .replace(/#|@/gi, '')
-      .split(this.separator)
-      .filter(s => s.length > 0)
-      .forEach(s => newVar.push(s));
-    return newVar;
-  }
-
-  pressEnter(e) {
-    return e.charCode === 13;
-  }
+  Suggestions() {}
 }

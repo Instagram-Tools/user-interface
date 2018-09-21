27240796825b73b3734ac6c6bbdd4a16463028ad
Gott50 <xxGott50xx@gmail.com>
2018-09-21 11:26:29 +0200

    log up to 5 arguments

--- src/Log.js
+++ src/Log.js
@@ -3,12 +3,62 @@ export default class Log {
     return process.env.NODE_ENV === 'development';
   }
 
-  static log(message, ...optionalParams) {
-    if (this.isEnabled()) console.log(message, optionalParams);
+  static log() {
+    if (this.isEnabled())
+      switch (arguments.length) {
+        case 1:
+          return console.log(arguments[0]);
+        case 2:
+          return console.log(arguments[0], arguments[1]);
+        case 3:
+          return console.log(arguments[0], arguments[1], arguments[2]);
+        case 4:
+          return console.log(
+            arguments[0],
+            arguments[1],
+            arguments[2],
+            arguments[3]
+          );
+        case 5:
+          return console.log(
+            arguments[0],
+            arguments[1],
+            arguments[2],
+            arguments[3],
+            arguments[4]
+          );
+        default:
+          return console.log(arguments);
+      }
   }
 
-  static error(message, ...optionalParams) {
-    if (this.isEnabled()) console.error(message, optionalParams);
+  static error() {
+    if (this.isEnabled())
+      switch (arguments.length) {
+        case 1:
+          return console.error(arguments[0]);
+        case 2:
+          return console.error(arguments[0], arguments[1]);
+        case 3:
+          return console.error(arguments[0], arguments[1], arguments[2]);
+        case 4:
+          return console.error(
+            arguments[0],
+            arguments[1],
+            arguments[2],
+            arguments[3]
+          );
+        case 5:
+          return console.error(
+            arguments[0],
+            arguments[1],
+            arguments[2],
+            arguments[3],
+            arguments[4]
+          );
+        default:
+          return console.error(arguments);
+      }
   }
 }
 

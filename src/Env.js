d640d3cf7caa07950b10d9a79c6fe6639a9943e4
Gott50 <xxGott50xx@gmail.com>
2018-09-25 21:32:26 +0200

    add .dev.json for Env in Development

--- src/Env.js
+++ src/Env.js
@@ -1,6 +1,7 @@
 let env = process.env;
 try {
-  env = require('./.env.json');
+  if (process.env === 'development') env = require('./.dev.json');
+  else env = require('./.env.json');
 } catch (e) {
   console.error(e);
   env = process.env;

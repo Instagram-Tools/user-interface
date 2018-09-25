4947916cc9ab46758a78b81b691acfcd0be76ede
Gott50 <xxGott50xx@gmail.com>
2018-09-25 21:38:18 +0200

    use .dev.json in process.env.NODE_ENV === 'development'

--- src/Env.js
+++ src/Env.js
@@ -1,6 +1,6 @@
 let env = process.env;
 try {
-  if (process.env === 'development') env = require('./.dev.json');
+  if (process.env.NODE_ENV === 'development') env = require('./.dev.json');
   else env = require('./.env.json');
 } catch (e) {
   console.error(e);

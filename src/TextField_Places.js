0734c00164ad7a80dd09877977af5fcb8e8038b3
Gott50 <xxGott50xx@gmail.com>
2018-09-26 20:29:20 +0200

    submit Suggestion vai onClick

--- src/TextField_Places.js
+++ src/TextField_Places.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TextFieldSuggestion from './TextFieldSuggestion';
+import { Context } from './Context';
 
 export default class TextField_Places extends TextFieldSuggestion {
   query = 'places';
@@ -7,18 +8,30 @@ export default class TextField_Places extends TextFieldSuggestion {
 
   mapSuggestions(suggestion) {
     return (
-      <div
-        onClick={() => this.clickSuggestion(suggestion.place.location.pk)}
-        style={{ display: 'inline-flex', justifyContent: 'space-between' }}
-      >
-        <div className="suggestion_text_locations">
-          {suggestion.place.location.name}
-        </div>
-      </div>
+      <Context.Consumer>
+        {context => (
+          <div
+            onClick={() =>
+              this.clickSuggestion(suggestion.place.location.pk, context)
+            }
+            style={{ display: 'inline-flex', justifyContent: 'space-between' }}
+          >
+            <div className="suggestion_text_locations">
+              {suggestion.place.location.name}
+            </div>
+          </div>
+        )}
+      </Context.Consumer>
     );
   }
 
   pressEnter() {
     return false;
+  }
+
+  clickSuggestion(suggestion, context) {
+    this.setState({ value: suggestion });
+    this.submitText(context);
+    this.setState({ suggestions: [] });
   }
 }

a9cbdf82f96ff19055842d625b89f5c3882565cc
Gott50 <xxGott50xx@gmail.com>
2018-09-27 09:01:03 +0200

    display suggestion.place.location.name via this.state.list

--- src/TextField_Places.js
+++ src/TextField_Places.js
@@ -29,11 +29,57 @@ export default class TextField_Places extends TextFieldSuggestion {
 
   async clickSuggestion(suggestion, context) {
     await this.setState({ value: suggestion.place.location.pk });
+    await this.setState(p => ({
+      list: [...(p.list || []), suggestion.place.location.name],
+      suggestions: []
+    }));
     await this.submitText(context);
-    await this.setState({ suggestions: [] });
   }
 
   addValue(old = [], value) {
     return [...old, value];
+  }
+
+  getList() {
+    return (
+      <Context.Consumer>
+        {context => (
+          <div className="columnholder applied_hashtasgs_and_locations">
+            {this.map(this.state.list, context)}
+          </div>
+        )}
+      </Context.Consumer>
+    );
+  }
+
+  map(list = [], context) {
+    return list.map((element, index) => (
+      <div key={index} className="applied_hashtag_or_location">
+        <div className="classicon">
+          <div className={this.props.iconClass} />
+        </div>
+        <div className="hashtag_or_location_text">{element}</div>
+        <div
+          className="deletebutton"
+          value={index}
+          onClick={() => this.remove(index, context)}
+        >
+          <div className="xicon" value={index} />
+        </div>
+      </div>
+    ));
+  }
+
+  remove(index, context) {
+    this.setState(p => {
+      let l = [...p.list];
+      l.splice(index, 1);
+      return { list: l };
+    });
+    context.setState(p => {
+      let l = [...p[this.props.value]];
+      l.splice(index, 1);
+      return { [this.props.value]: l };
+    });
   }
 }

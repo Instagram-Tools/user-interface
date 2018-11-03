8f3b489c4c66d1ebd9fef3d72ca6249f25d3ce26
Gott50 <xxGott50xx@gmail.com>
2018-11-03 11:51:44 +0100

    add LandingPage_Trail

---
+++ src/LandingPage_Trail.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+export default class LandingPage_Trail extends Component {
+  render() {
+    return (
+      <div className="connect_insta_account landing_page_trial">
+        <h1 className="settingtitle">Test Pink Parrot for free for 7 days</h1>
+        <div className="title_menu_element trialtext">
+          This is our exclusive launch offer, expiring on November 1:<br />You
+          get to test the full Pink Parrot software suit for free.<br />And
+          don&#x27;t even have to enter your payment details.
+        </div>
+        <div className="w-form">
+          <form
+            id="email-form-2"
+            name="email-form-2"
+            data-name="Email Form 2"
+            className="formholder_connect_account"
+          >
+            <input
+              type="submit"
+              value="Start trial"
+              data-wait="Please wait..."
+              className="submitbutton connect_account_button trialbutton w-button"
+            />
+            <input
+              type="submit"
+              value="purchase subscription"
+              data-wait="Please wait..."
+              className="submitbutton connect_account_button buy_anyway_button w-button"
+            />
+          </form>
+          <div className="w-form-done">
+            <div>Thank you! Your submission has been received!</div>
+          </div>
+          <div className="w-form-fail">
+            <div>Oops! Something went wrong while submitting the form.</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}

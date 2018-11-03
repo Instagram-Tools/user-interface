ce74309e20b0f54846ca45af55fe240615e39441
Gott50 <xxGott50xx@gmail.com>
2018-11-03 10:13:31 +0100

    wrap Router with Context Provider to provide Context

--- src/App.js
+++ src/App.js
@@ -11,48 +11,45 @@ import FollowSettings from './FollowSettings';
 import UnfollowSettings from './UnfollowSettings';
 // import MessageSettings from './MessageSettings';
 import ActivitySettings from './ActivitySettings';
-import { Context, Provider } from './Context';
+import { Context } from './Context';
 import Save from './Save';
+import Payment_Gateway from './Payment_Gateway';
 
 export default class App extends Component {
   render() {
     return (
-      <Provider>
-        <Context.Consumer>
-          {context => (
-            <div>
-              <Progress />
-              <NavBar />
-              <GeneralSettings />
-              <LikeSettings
-                className={
-                  'hideable ' + (context.state.do_like_enabled ? '' : 'hidden')
-                }
-              />
-              <CommentSettings
-                className={
-                  'hideable ' +
-                  (context.state.do_comment_enabled ? '' : 'hidden')
-                }
-              />
-              <FollowSettings
-                className={
-                  'hideable ' +
-                  (context.state.do_follow_enabled ? '' : 'hidden')
-                }
-              />
-              <UnfollowSettings
-                className={
-                  'hideable ' + (context.state.enable_unfollow ? '' : 'hidden')
-                }
-              />
-              {/*<MessageSettings />*/}
-              <ActivitySettings />
-              <Save />
-            </div>
-          )}
-        </Context.Consumer>
-      </Provider>
+      <Context.Consumer>
+        {context => (
+          <div>
+            <Progress />
+            <NavBar />
+            <GeneralSettings />
+            <LikeSettings
+              className={
+                'hideable ' + (context.state.do_like_enabled ? '' : 'hidden')
+              }
+            />
+            <CommentSettings
+              className={
+                'hideable ' + (context.state.do_comment_enabled ? '' : 'hidden')
+              }
+            />
+            <FollowSettings
+              className={
+                'hideable ' + (context.state.do_follow_enabled ? '' : 'hidden')
+              }
+            />
+            <UnfollowSettings
+              className={
+                'hideable ' + (context.state.enable_unfollow ? '' : 'hidden')
+              }
+            />
+            {/*<MessageSettings />*/}
+            <ActivitySettings />
+            <Save />
+          </div>
+        )}
+      </Context.Consumer>
     );
   }
 }

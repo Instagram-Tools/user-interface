701023fe995b0669f4a009a0b5dda45ff9117994
Gott50 <xxGott50xx@gmail.com>
2018-11-11 15:21:27 +0100

    add LandingPage_Register with TextField_Confirm

---
+++ src/LandingPage_Register.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import TextField from './TextField';
+import TextField_Confirm from './TextField_Confirm';
+
+export default class LandingPage_Register extends Component {
+  state = { isConfirmed: false };
+
+  render() {
+    return (
+      <div
+        style={this.props.display ? { opacity: 1, display: 'unset' } : {}}
+        className="connect_insta_account landing_page_connect"
+      >
+        <h1 className="settingtitle">Connect your Instagram account</h1>
+        <a href="/privacy-policy" className="title_menu_element privacy">
+          Your details will not be transferred to third parties, and neither
+          your email, nor your password will be saved. Click to see our privacy
+          policy.
+        </a>
+        <div className="w-form">
+          <form
+            id="email-form-2"
+            name="email-form-2"
+            data-name="Email Form 2"
+            className="formholder_connect_account"
+          >
+            <div className="columnholder filterholder first connect_account_column">
+              <TextField type="email" placeholder="Email" value="email" />
+              <TextField
+                type="password"
+                placeholder="Password"
+                value="e_password"
+              />
+              <TextField_Confirm
+                type="password"
+                placeholder="Confirm Password"
+                value="e_password"
+                setIsConfirmed={this.setIsConfirmed.bind(this)}
+              />
+            </div>
+            <input
+              onClick={this.state.isConfirmed ? this.props.toggle : null}
+              type="button"
+              value="Connect"
+              data-wait="Please wait..."
+              className="submitbutton connect_account_button w-button"
+            />
+          </form>
+          <div className="w-form-done">
+            <div>Thank you! Your submission has been received!</div>
+          </div>
+          <div className="w-form-fail">
+            <div>Oops! Something went wrong while submitting the form.</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  setIsConfirmed(isConfirmed = false) {
+    this.setState({ isConfirmed });
+  }
+}

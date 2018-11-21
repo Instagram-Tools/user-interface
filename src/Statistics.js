fe62be9841d7b167d7e54a79f16c18958e472f01
Gott50 <xxGott50xx@gmail.com>
2018-11-21 13:29:45 +0100

    remove Text from Statistics

--- src/Statistics.js
+++ src/Statistics.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 export default class Statistics extends Component {
   render() {
-    return (
-      <div className={this.props.className}>Liked: Commented: Followed:</div>
-    );
+    return <div className={this.props.className} />;
   }
 }

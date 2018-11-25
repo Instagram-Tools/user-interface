d1828a61b22dda4a59b70c757d5093db60b39d34
Gott50 <xxGott50xx@gmail.com>
2018-11-25 13:09:20 +0100

    add BOT_Gateway.js

---
+++ src/BOT_Gateway.js
@@ -0,0 +1,24 @@
+import env from './Env';
+
+const BOT_URL = env.BOT_URL || document.location.origin + ':8765/bot';
+export default class BOT_Gateway {
+  static async fetchText(url) {
+    let response = await fetch(BOT_URL + url);
+    let text = await response.text();
+    if (response.status === 200) return text;
+    else return new Error(text);
+  }
+
+  static async isBotRunning(username) {
+    let text = await this.fetchText(`/${username}`);
+    return text === 'True';
+  }
+
+  static async stopBot(username) {
+    return await this.fetchText(`/stop/${username}`);
+  }
+
+  static async startBot(username) {
+    return await this.fetchText(`/start/${username}`);
+  }
+}

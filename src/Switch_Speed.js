51b9e38a5a6f938e1b7b5f2ba6606a2a34e28b53
Gott50 <xxGott50xx@gmail.com>
2018-11-26 18:49:40 +0100

    fix Switch_Speed

--- src/Switch_Speed.js
+++ src/Switch_Speed.js
@@ -9,43 +9,41 @@ export default class Switch_Speed extends Component {
       <Context.Consumer>
         {context => (
           <div className="switch_general_settings title speedswitch">
-            <div className="switch_general_settings title speedswitch_text_holder">
-              <div
-                style={this.activate(
-                  context.state[this.props.value],
-                  this.speed.slow
-                )}
-                onClick={(e => this.setSpeed(context, this.speed.slow)).bind(
-                  this
-                )}
-                className="speedtext slowspeed"
-              >
-                slow
-              </div>
-              <div
-                style={this.activate(
-                  context.state[this.props.value],
-                  this.speed.medium
-                )}
-                onClick={(e => this.setSpeed(context, this.speed.medium)).bind(
-                  this
-                )}
-                className="speedtext mediumspeed"
-              >
-                medium
-              </div>
-              <div
-                style={this.activate(
-                  context.state[this.props.value],
-                  this.speed.fast
-                )}
-                onClick={(e => this.setSpeed(context, this.speed.fast)).bind(
-                  this
-                )}
-                className="speedtext fastspeed"
-              >
-                fast
-              </div>
+            <div
+              style={this.activate(
+                context.state[this.props.value],
+                this.speed.slow
+              )}
+              onClick={(e => this.setSpeed(context, this.speed.slow)).bind(
+                this
+              )}
+              className="speedtext slowspeed"
+            >
+              slow
+            </div>
+            <div
+              style={this.activate(
+                context.state[this.props.value],
+                this.speed.medium
+              )}
+              onClick={(e => this.setSpeed(context, this.speed.medium)).bind(
+                this
+              )}
+              className="speedtext mediumspeed"
+            >
+              medium
+            </div>
+            <div
+              style={this.activate(
+                context.state[this.props.value],
+                this.speed.fast
+              )}
+              onClick={(e => this.setSpeed(context, this.speed.fast)).bind(
+                this
+              )}
+              className="speedtext fastspeed"
+            >
+              fast
             </div>
           </div>
         )}

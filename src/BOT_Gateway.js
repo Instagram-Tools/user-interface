204c1ae2884c075f257659463094d54f2cd58c8a
Gott50 <xxGott50xx@gmail.com>
2018-11-26 18:15:29 +0100

    fix BOT_Gateway

--- src/BOT_Gateway.js
+++ src/BOT_Gateway.js
@@ -4,11 +4,14 @@ import env from './Env';
 const BOT_URL = env.BOT_URL || document.location.origin + '/bot';
 export default class BOT_Gateway {
   static async fetchText(url) {
-    console.log(url);
-    let response = await fetch(BOT_URL + url);
-    let text = await response.text();
-    if (response.status === 200) return text;
-    else return new Error(text);
+    try {
+      let response = await fetch(BOT_URL + url);
+      let text = await response.text();
+      if (response.status === 200) return text;
+      else return new Error(text);
+    } catch (e) {
+      return e;
+    }
   }
 
   static async isBotRunning(username) {

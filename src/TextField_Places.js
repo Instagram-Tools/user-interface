3699918c147622e75f5752c112a959274401fc12
Gott50 <xxGott50xx@gmail.com>
2018-12-02 21:58:24 +0100

    rename context.state.list to context.state.location_names

--- src/TextField_Places.js
+++ src/TextField_Places.js
@@ -25,7 +25,10 @@ export default class TextField_Places extends TextFieldSuggestion {
   async clickSuggestion(suggestion, context) {
     await this.setState({ value: suggestion.place.location.pk });
     await context.setState(p => ({
-      list: [...(p.list || []), suggestion.place.location.name]
+      location_names: [
+        ...(p.location_names || []),
+        suggestion.place.location.name
+      ]
     }));
     await this.submitText(context);
     this.textInput.focus();
@@ -35,16 +38,16 @@ export default class TextField_Places extends TextFieldSuggestion {
     return [...old, value];
   }
 
-  map(list = [], context) {
-    return super.map(context.state.list, context);
+  map(location_names = [], context) {
+    return super.map(context.state.location_names, context);
   }
 
   remove(index, context) {
     super.remove(index, context);
     context.setState(p => {
-      let l = [...p.list];
+      let l = [...p.location_names];
       l.splice(index, 1);
-      return { list: l };
+      return { location_names: l };
     });
   }
 }

c8b1f7e4621824e31f2207c412128f7a42d141ad
Gott50 <xxGott50xx@gmail.com>
2018-12-16 00:48:13 +0100

    fix: ./src/LandingPageSuccess.js        Line 2:   'TextField' is defined but never used                                                                                       no-unused-vars        Line 3:   'PaymentGateway' is defined but never used                                                                                  no-unused-vars        Line 4:   'Context' is defined but never used                                                                                         no-unused-vars

--- src/LandingPageSuccess.js
+++ src/LandingPageSuccess.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import TextField from './TextField';
-import PaymentGateway from './PaymentGateway';
-import { Context } from './Context';
 
 export default class LandingPageSuccess extends Component {
   render() {

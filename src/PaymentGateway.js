ff815a15c5a0dce2c8b1137557202b675cae87f0
Gott50 <xxGott50xx@gmail.com>
2019-01-08 21:49:28 +0100

    extract STRIPE_PUBLIC_KEY to Env

--- src/PaymentGateway.js
+++ src/PaymentGateway.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 import DropIn from './DropIn';
 
+import env from './Env';
+
+const STRIPE_PUBLIC_KEY = env.STRIPE_PUBLIC_KEY;
+
 export default class PaymentGateway extends React.Component {
   render() {
     return (
-      <StripeProvider apiKey="pk_test_b1xnLwuXAD8nz71YQQX2ygaP">
+      <StripeProvider apiKey={STRIPE_PUBLIC_KEY}>
         <Elements>
           <DropIn setSubscription={this.props.setSubscription} />
         </Elements>

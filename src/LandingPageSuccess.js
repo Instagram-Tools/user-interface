86d1ce7d3b19c40e6d9ae7f31d0fd2853cf3245b
Gott50 <xxGott50xx@gmail.com>
2019-02-05 19:53:02 +0100

    update LandingPageSuccess classNames

--- src/LandingPageSuccess.js
+++ src/LandingPageSuccess.js
@@ -6,48 +6,17 @@ export default class LandingPageSuccess extends Component {
       <div
         style={
           this.props.display
-            ? { display: 'flex', position: 'absolute', margin: 0 }
+            ? { opacity: 1, display: 'flex' }
             : { display: 'none' }
         }
-        className="connect_insta_account landing_page_payment successmessage"
+        className="connect_insta_account"
       >
-        <h1 className="settingtitle subscribetitle">
-          We're pumped to welcome you to Pink Parrot! 😏
+        <h1 id="w-node-b0d9bd887e77-3645cebf" className="logintitle">
+          We&#x27;re pumped to welcome you to Pink Parrot!😏
         </h1>
-        <div className="successimage">
-          <div className="paycolumn right_pay_column">
-            <div className="envelopeholder">
-              <img
-                src="images/Asset-57-target.png"
-                srcSet="images/Asset-57-target-p-500.png 500w, images/Asset-57-target-p-800.png 800w, images/Asset-57-target-p-1080.png 1080w, images/Asset-57-target.png 1423w"
-                sizes="100vw"
-                alt=""
-                className="image-2-copy-copy"
-              />
-              <img
-                src="images/Envelope-Foreground.svg"
-                alt=""
-                className="image-2-copy"
-              />
-              <img
-                src="images/Envelope-Shadow.png"
-                srcSet="images/Envelope-Shadow-p-500.png 500w, images/Envelope-Shadow-p-800.png 800w, images/Envelope-Shadow-p-1080.png 1080w, images/Envelope-Shadow-p-1600.png 1600w, images/Envelope-Shadow-p-2000.png 2000w, images/Envelope-Shadow.png 2496w"
-                sizes="100vw"
-                alt=""
-                className="image-2"
-              />
-              <img
-                src="images/Envelope-Notifications.svg"
-                alt=""
-                className="image"
-              />
-            </div>
-          </div>
-        </div>
-        <button
-          onClick={this.props.toggle}
-          className="submitbutton connect_account_button get_started_button w-button"
-        >
+        <div className="loginsubtitle">Always press save.</div>
+        <div id="w-node-b0d9bd887e7b-3645cebf" className="successimage" />
+        <button onClick={this.props.toggle} className="loginbutton w-button">
           Get Started
         </button>
       </div>

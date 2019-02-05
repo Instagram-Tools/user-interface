57b63c926f5591f6b3bb74699464442d15689edd
Gott50 <xxGott50xx@gmail.com>
2019-02-05 17:46:56 +0100

    update LandingPageConnect classNames

--- src/LandingPageConnect.js
+++ src/LandingPageConnect.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import TextField from './TextField';
-import { Link } from 'react-router-dom';
 import API from './API_Gateway';
 import console from './Log';
 import { Context } from './Context';
+import { NavHashLink } from 'react-router-hash-link';
 
 export default class LandingPageConnect extends Component {
   state = {
@@ -20,27 +20,33 @@ export default class LandingPageConnect extends Component {
             <div
               style={
                 this.props.display
-                  ? { opacity: 1, display: 'unset' }
+                  ? { opacity: 1, display: 'flex' }
                   : { display: 'none' }
               }
-              className="connect_insta_account landing_page_payment connect"
+              className="connect_insta_account"
             >
-              <h1 className="settingtitle">Connect your Instagram account</h1>
-              <Link to="/privacy-policy" className="title_menu_element privacy">
-                Your details will not be transferred to third parties, and
-                neither your email, nor your password will be saved. Click to
-                see our privacy policy.
-              </Link>
+              <h1 className="logintitle">Connect your Instagram account</h1>
+              <NavHashLink
+                to="/leagal/#privacy"
+                className="loginsubtitle clickprivacy"
+              >
+                Your details will not be transferred to third parties. Your
+                password is encrypted. Click to see our privacy policy.
+              </NavHashLink>
               <div className="w-form">
                 <div className="formholder_connect_account">
                   <div className="columnholder filterholder first connect_account_column">
                     <TextField
+                      inputClass="text-field-2 filter connect_account_filter w-input"
+                      className="text_field_description-2 below connect_account_text"
                       type="email"
                       placeholder="Instagram Username"
                       value="username"
                       setIsSet={b => this.setState({ isEmailSet: b })}
                     />
                     <TextField
+                      inputClass="text-field-2 filter connect_account_filter w-input"
+                      className="text_field_description-2 below connect_account_text"
                       type="password"
                       placeholder="Instagram Password"
                       value="password"
@@ -52,7 +58,7 @@ export default class LandingPageConnect extends Component {
                     type="button"
                     value="Connect"
                     data-wait="Please wait..."
-                    className="submitbutton connect_account_button w-button"
+                    className="loginbutton w-button"
                   />
                 </div>
                 <div className="w-form-done">
@@ -60,7 +66,7 @@ export default class LandingPageConnect extends Component {
                 </div>
                 <div
                   style={this.state.error ? { display: 'block' } : {}}
-                  className="w-form-fail"
+                  className="error-message-3 w-form-fail"
                 >
                   <div>
                     Oops! Something went wrong while submitting the form.

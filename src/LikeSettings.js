3194e21abe9f97d5e1fc5b0440c7f787b0d0a80c
Gott50 <xxGott50xx@gmail.com>
2019-02-19 13:49:48 +0100

    cleanup: remove old <label> and <Help> of LikeSettings

--- src/LikeSettings.js
+++ src/LikeSettings.js
@@ -13,20 +13,6 @@ export default class LikeSettings extends Component {
         <div data-w-id="885ad75b-5927-d36b-7821-9522271a2279" className="body">
           <div className="w-form form">
             <div className="titleholder">
-              <Help
-                data-w-id="22c58d28-9a42-f414-7b98-09bcc5c05a9b"
-                className="helptext"
-              >
-                <strong>Like X percent of crawled pictures: </strong> <br />
-                100% means every crawled picture will be liked. <br />
-                50% means every second crawled picture will be liked.
-              </Help>
-              <label htmlFor="email" className="settingtitle">
-                Like Percentage
-              </label>
-            </div>
-            {/*<span> equals 700 - 1050 likes/day </span>*/}
-            <div className="titleholder">
               <Help className="helptext">
                 <strong>Like pictures with...: </strong>
                 <br />

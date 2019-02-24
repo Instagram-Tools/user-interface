7fd6c58bedf0e7810de99ed9488bb64bedf52649
Gott50 <xxGott50xx@gmail.com>
2019-02-24 12:37:16 +0100

    refactor: cleanup Comments

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ import Sections from './Sections';
 import GeneralSettings from './GeneralSettings';
 import LikeSettings from './LikeSettings';
 import CommentSettings from './CommentSettings';
-// import FollowSettings from './FollowSettings';
 import UnfollowSettings from './UnfollowSettings';
-// import MessageSettings from './MessageSettings';
 import ActivitySettings from './ActivitySettings';
 import { Context } from './Context';
 import Save from './Save';
@@ -34,21 +32,11 @@ export default class App extends Component {
                 'hideable ' + (context.state.do_comment_enabled ? '' : 'hidden')
               }
             />
-            {/*<FollowSettings*/}
-            {/*className={*/}
-            {/*'hideable ' + (context.state.do_follow_enabled ? '' : 'hidden')*/}
-            {/*}*/}
-            {/*/>*/}
             <UnfollowSettings
               className={
                 'hideable ' + (context.state.enable_unfollow ? '' : 'hidden')
               }
             />
-            {/*<MessageSettings*/}
-            {/*className={*/}
-            {/*'hideable ' + (context.state.enable_message ? '' : 'hidden')*/}
-            {/*}*/}
-            {/*/>*/}
             <ActivitySettings />
             <Save />
           </div>

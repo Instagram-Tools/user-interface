5d8258d9d63e5ba7c9745b4ee9893e6d7b0964e6
Gott50 <xxGott50xx@gmail.com>
2019-03-21 12:42:17 +0100

    use serviceWorker.unregister()

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ import * as serviceWorker from './serviceWorker';
 import Routing from './Routing';
 
 ReactDOM.render(<Routing />, document.getElementById('root'));
+serviceWorker.unregister();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register({
+//   onUpdate: registration => {
+//     console.log('onUpdate() registration', registration);
+//     alert('New content is available and will be used when all tabs for this page are closed.');
+//     window.close();
+//   }
+// });
